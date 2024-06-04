d940eb274575f76ca58631661a3f9a43a32f52e4
GitKIY <storage.GitHub.KIY@gmail.com>
2024-06-04 16:45:09 +0300

    commit3

--- src/main/java/ru/example/Starter.java
+++ src/main/java/ru/example/Starter.java
@@ -3,32 +3,31 @@ package ru.example;
 public class Starter {
     public static void main(String[] args) {
         Account acc1= new Account();
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         acc1.setAmtArr(ECurrency.RUB,100);
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         acc1.setOwner("Василий Иванов");
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         acc1.setAmtArr(ECurrency.USD,300);
-        System.out.println("\n"+acc1.toString()+"\n");
+        System.out.println("\n"+acc1+"\n");
 
         if (acc1.canUndo()) acc1.undo();
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         if (acc1.canUndo()) acc1.undo();
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         if (acc1.canUndo()) acc1.undo();
-        System.out.println(acc1.toString()+"\n\n");
+        System.out.println(acc1+"\n\n");
 
         System.out.println("------------------ SAVE ---------------------");
         acc1.setOwner("Василий Иванов");
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         acc1.setAmtArr(ECurrency.RUB,300);
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
 
         acc1.save();
         System.out.println(acc1.savedAcc.toString()+"<------ Сохраненное значение");
         if (acc1.canUndo()) acc1.undo();
-        System.out.println(acc1.toString());
+        System.out.println(acc1);
         System.out.println(acc1.savedAcc.toString()+"<------ Сохраненное значение");
-        System.out.println("----------");
     }
 }
